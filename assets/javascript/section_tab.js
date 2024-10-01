7ac452e1bdaf25fc81cc666d9cff9c9618f31c1e
ebolox <enrico.bologna@yahoo.it>
2024-10-01 11:14:10 +0200

    feat(tabs): funzione js mostra nasconde subform

--- assets/javascript/section_tab.js
+++ assets/javascript/section_tab.js
@@ -17,4 +17,25 @@ $(document).ready( function () {
   // Gestione icona edit
   $(".btn-edit").click( function () { switch_to_edit(event, $(this).next()); });
   $(".btn-edit + input[type='text']").blur( function () { switch_to_edit(event, $(this)); });
+
+  // Mostra/nasconde la sezione
+  $("ul.form-label > li.form-title").click( function () {
+
+    form_label = $(this).parent();
+    form_name = form_label.data("toggle");
+    form_data = $("ul.form-data[data-toggle='" + form_name + "']");
+    form_toggle = $(this).find("i");
+
+    if (form_label.attr("class").match("hidden")) {
+
+      form_toggle.removeClass("bi-chevron-down").addClass("bi-chevron-up");
+      form_label.removeClass("hidden");
+      form_data.removeClass("hidden");
+    } else {
+
+      form_toggle.removeClass("bi-chevron-up").addClass("bi-chevron-down");
+      form_label.addClass("hidden");
+      form_data.addClass("hidden");
+    }
+  });
 });
