8eb8044849814505c6230a01f0d7f7a085351b06
ebolox <enrico.bologna@yahoo.it>
2024-10-01 11:21:29 +0200

    refact(book): condizione method get per chiamate parziali

--- book.php
+++ book.php
@@ -7,15 +7,23 @@
 
   // Sezione da mostrare
   $section = get_section_to_view((isset($_GET) && !empty($_GET["section"])) ? $_GET["section"] : "club");
+
+  if ($_SERVER["REQUEST_METHOD"] === "GET") {
 ?>
   <div id="<?= $model ?>_content" class="vb-content vb-list mt-4">
 
     <?= form_navbar($model, array("print", "new", "delete"), $section) ?>
 
     <div id="<?= $model ?>_list">
+<?php
+  }
+?>
 
     <?= get_book($section) ?>
 
+<?php
+  if ($_SERVER["REQUEST_METHOD"] === "GET") {
+?>
     </div>
   </div>
   <script>
@@ -23,3 +31,6 @@
     var section = "<?= $section ?>";
   </script>
   <script src="assets/javascript/section_list.js"></script>
+<?php
+  }
+?>
